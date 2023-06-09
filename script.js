a760ade1ba70f5071f1a9a76cf8a8c1d2c865522
Jared <gorillacyborgz@gmail.com>
2023-06-09 19:32:41 +0800

    Updated files.

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-let searchForm = document.getElementById("searchForm");
-let content = document.querySelector(".content");
-
-const apiKey = "529676f7f86af93bf3cc52b1dff741b9";
-const input = searchForm.value;
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=metric`;
+const searchForm = document.getElementById("searchForm");
+const content = document.querySelector(".content");
+const city = document.querySelector(".location");
+const img = document.querySelector(".icon");
+const conditions = document.querySelector(".conditions");
+const temp = document.querySelector(".temp");
+const feelsLike = document.querySelector(".feeltemp");
+const humidity = document.querySelector(".humidity");
+const high = document.querySelector(".high");
+let x = 100;
 
 searchForm.addEventListener("submit", (e)=>{
     e.preventDefault();
+
+    const apiKey = "fe982205ce99e8f378cf9ecceb2dcc23";
+    const input = search.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=${apiKey}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data =>{
+        const {main, name, sys, weather, rain, pressure} = data;
+        const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+        city.textContent = `${name}`;
+        img.src = `${icon}`;
+        conditions.textContent = `${weather[0]["description"]}`;
+        temp.textContent = `${Math.round(main.temp)}°C`;
+        feelsLike.textContent = `Feels Like: ${Math.round(main.feels_like)}°C`;
+        high.textContent = `Daily High: ${Math.round(main.temp_max)}°C`;
+        humidity.textContent = `Humidity: ${main.humidity}%`;
+    })
+    .catch(() => {
+        alert("call failed.");
+    })
+
     fadeIn(content);
 })
 
-fetch(url)
-    .then(response => response.json())
-    .then(data =>{
 
-    })
 function fadeIn(content){
     var op = 0.1;
     content.style.display = 'block';
